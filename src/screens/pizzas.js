f028f0127f1d17869f078bc5b3d943c2862ae0fd
Gui <guifelixs@gmail.com>
2024-11-07 18:50:19 -0300

    adicionando pizzas na tela

--- src/screens/pizzas.js
+++ src/screens/pizzas.js
@@ -1,10 +1,21 @@
-import { View } from "react-native"
+import { View, ScrollView, Text } from "react-native"
 import SearchBar from "../components/searchbar/searchbar"
+import styles from "./style"
+import FoodCard from "../components/foodcard/foodcard"
+import FoodCardDoce from "../components/foodcard/foodcarddoce"
 
-export default function Pizzas(){
-    return(
-        <View style={{flex:1, justifyContent: 'center', alignContent:'center', padding:32}}>
-            <SearchBar/>
-        </View>
+export default function Pizzas() {
+    return (
+        <ScrollView style={styles.container}>
+            <SearchBar style={styles.seachBar} />
+            <Text style={styles.titulos}>
+                Pizzas salgadas
+            </Text>
+            <FoodCard />
+            <Text style={styles.titulos}>
+                Pizzas doces
+            </Text>
+            <FoodCardDoce />
+        </ScrollView>
     )
 }
