1f850b3a30613a45926d65194d89fd8940472dd9
Gui <guifelixs@gmail.com>
2024-11-06 22:19:28 -0300

    estilizando a searchBar

---
+++ src/components/searchbar/style.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 50,
+    },
+
+    searchBarStyle: {
+        width: '90%',
+        height: 70,
+        paddingHorizontal: 10,
+        borderRadius: 50,
+    }
+})
+
+export default styles;
