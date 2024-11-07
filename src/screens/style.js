6174ca18958b28e2afeccc00f3b23d6694293610
Gui <guifelixs@gmail.com>
2024-11-07 18:50:58 -0300

    criando estilo para view e titulos

---
+++ src/screens/style.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    
+    titulos: {
+        fontSize: 20,
+        color: '#006400',
+        padding: 20,
+        textAlign: 'left',
+    }
+})
+
+export default styles;
