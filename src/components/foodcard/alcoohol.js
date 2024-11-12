b9081218d2c54fd142065c60b795daca7fabf66c
Gui <guifelixs@gmail.com>
2024-11-11 21:51:58 -0300

    Adicionando TouchableOpacity no drinks

--- src/components/foodcard/alcoohol.js
+++ src/components/foodcard/alcoohol.js
@@ -1,21 +1,30 @@
 import * as React from 'react';
-import { Card, Button, Text } from 'react-native-paper';
-import { View } from 'react-native';
+import { Card } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity } from 'react-native';
 import styles from './style';
 
-const AlcoholDrinks = () => (
-    <View style={styles.container}>
-        <Card style={styles.cardView}>
-            <Card.Title title="Mojito" subtitle='350ml'/>
-            <Card.Cover source={{ uri: 'https://cdn.diffords.com/contrib/stock-images/2020/07/5f0ec76d62ff3.jpg' }}
-                style={styles.imgFood} />
-        </Card>
-        <Card style={styles.cardView}>
-            <Card.Title title="Tropical GIN" subtitle='300ml'/>
-            <Card.Cover source={{ uri: 'https://www.vatrockbar.com.br/wp-content/uploads/2023/12/tropical-gin.png' }}
-                style={styles.imgFood} />
-        </Card>
-    </View>
-);
+const AlcoholDrinks = () => {
+    const navigation = useNavigation();
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.navigate('Coca-Cola')} style={styles.touchableContainer}>
+                <Card style={styles.cardView}>
+                    <Card.Title title="Mojito" subtitle='350ml' />
+                    <Card.Cover source={{ uri: 'https://cdn.diffords.com/contrib/stock-images/2020/07/5f0ec76d62ff3.jpg' }}
+                        style={styles.imgFood} />
+                </Card>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('DetailsCoca')} style={styles.touchableContainer}>
+                <Card style={styles.cardView}>
+                    <Card.Title title="Tropical GIN" subtitle='300ml' />
+                    <Card.Cover source={{ uri: 'https://www.vatrockbar.com.br/wp-content/uploads/2023/12/tropical-gin.png' }}
+                        style={styles.imgFood} />
+                </Card>
+            </TouchableOpacity>
+        </View>
+    )
+};
 
 export default AlcoholDrinks;
