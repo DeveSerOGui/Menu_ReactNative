cf9ce85253e1987da5a85053143966c7f06c4415
Gui <guifelixs@gmail.com>
2024-11-11 21:53:45 -0300

    Criando a primeira página de detalhes da bebida

---
+++ src/screens/detailsPage/detailsBebidas/cocacola/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View  } from 'react-native'
+
+export default function CocaCola() {
+    return (
+        <View>
+            <Text>
+                Detalhe coca
+            </Text>
+        </View>
+    )
+};
