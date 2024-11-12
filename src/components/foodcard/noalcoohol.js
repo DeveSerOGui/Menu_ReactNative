53bac0c64a0d333ae32e70b076a512b723b622df
Gui <guifelixs@gmail.com>
2024-11-11 21:52:41 -0300

    add TouchableOpacity  e Navigation nas bebidas

--- src/components/foodcard/noalcoohol.js
+++ src/components/foodcard/noalcoohol.js
@@ -1,36 +1,39 @@
 import * as React from 'react';
-import { Card, Button, Text } from 'react-native-paper';
-import { View } from 'react-native';
+import { Card } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity } from 'react-native';
 import styles from './style';
 
-const NoalcoholDrinks = () => (
-    <View style={styles.container}>
-        <Card style={styles.cardView}>
-            <Card.Title title="Coca-cola" subtitle='350ml'/>
-            <Card.Cover source={{ uri: 'https://drdrinksusa.com/cdn/shop/products/Coke_grande.jpg?v=1546134439' }}
-                style={styles.imgFood} />
-        </Card>
-        <Card style={styles.cardView}>
-            <Card.Title title="Mate Leão" subtitle='300ml'/>
-            <Card.Cover source={{ uri: 'https://superprix.vteximg.com.br/arquivos/ids/206618-600-600/733776-LEAO_MATTE_PET_300ML.jpg?v=637793410681300000' }}
-                style={styles.imgFood} />
-        </Card>
-    </View>
-);
+const NoalcoholDrinks = () => {
+    const navigation = useNavigation();
 
-const AlcoholDrinks = () => (
-    <View style={styles.container}>
-        <Card style={styles.cardView}>
-            <Card.Title title="Coca-cola" subtitle='350ml'/>
-            <Card.Cover source={{ uri: 'https://drdrinksusa.com/cdn/shop/products/Coke_grande.jpg?v=1546134439' }}
-                style={styles.imgFood} />
-        </Card>
-        <Card style={styles.cardView}>
-            <Card.Title title="Mate Leão" subtitle='300ml'/>
-            <Card.Cover source={{ uri: 'https://superprix.vteximg.com.br/arquivos/ids/206618-600-600/733776-LEAO_MATTE_PET_300ML.jpg?v=637793410681300000' }}
-                style={styles.imgFood} />
-        </Card>
-    </View>
-);
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.navigate('CocaCola')} style={styles.touchableContainer}>
+                <Card style={styles.cardView}>
+                    <Card.Title title="Coca-Cola" subtitle="350ml" />
+                    <Card.Cover
+                        source={{
+                            uri: 'https://drdrinksusa.com/cdn/shop/products/Coke_grande.jpg?v=1546134439',
+                        }}
+                        style={styles.imgFood}
+                    />
+                </Card>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('DetailsCoca')} style={styles.touchableContainer}>
+                <Card style={styles.cardView}>
+                    <Card.Title title="Mate Leão" subtitle="300ml" />
+                    <Card.Cover
+                        source={{
+                            uri: 'https://superprix.vteximg.com.br/arquivos/ids/206618-600-600/733776-LEAO_MATTE_PET_300ML.jpg?v=637793410681300000',
+                        }}
+                        style={styles.imgFood}
+                    />
+                </Card>
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 export default NoalcoholDrinks;
