6a96763c2340ec75ce54ac29b5c8a5f1724e2eaa
Gui <guifelixs@gmail.com>
2024-11-13 18:54:28 -0300

    Criando navegação de rotas bebidas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Bebidas from '../screens/bebidas';
 import Pizzas from '../screens/pizzas';
-import CocaCola from '../screens/detailsPage/detailsBebidas/cocacola';
+import CocaColaDetails from '../screens/detailsPage/detailsBebidas/cocacola';
+import MateLeaoDetails from '../screens/detailsPage/detailsBebidas/mateleao';
+import MojitoDetails from '../screens/detailsPage/detailsBebidas/mojito';
+import TropicalGinDetails from '../screens/detailsPage/detailsBebidas/tropicalgin';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Entypo from '@expo/vector-icons/Entypo';
 
@@ -15,7 +18,10 @@ function BebidasStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Bebidas" component={Bebidas} />
-      <Stack.Screen name="CocaCola" component={CocaCola} options={{ title: 'Detalhes da Coca-Cola' }}/>
+      <Stack.Screen name="CocaColaDetails" component={CocaColaDetails} options={{ title: 'Detalhes Coca-Cola' }}/>
+      <Stack.Screen name="MateLeaoDetails" component={MateLeaoDetails} options={{ title: 'Detalhes Mate-Leão' }}/>
+      <Stack.Screen name="MojitoDetails" component={MojitoDetails} options={{ title: 'Detalhes Mojito' }}/>
+      <Stack.Screen name="TropicalGinDetails" component={TropicalGinDetails} options={{ title: 'Detalhes da Tropical Gin' }}/>
     </Stack.Navigator>
   );
 }
